99c3a7b116e87d311f248f26733f0d2c49bed349
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-03-30 14:01:03 -0300

    add header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,31 +1,35 @@
 import React from "react";
 import garchomp from "../../assets/garchomp.png";
+import "./header.css";
 
 export default function Header() {
   return (
-    <div className="h-[100vh] w-full flex items-center justify-evenly bg-indigo-900 max-[600px]:inline">
-      <div className="w-[50%] ">
-        <div className=" text-white w-[80%]">
-          <h1 className="text-[80px]">GARCHOMP</h1>
-          <p className="text-[20px]">
+    <div className="garchomp-container h-screen w-full flex items-center justify-evenly bg-indigo-900">
+      <div className="w-[40%] garchomp-info">
+        <div className="max-[600px]:text-green-800 ">
+          <h1 className="titulo-garchomp">GARCHOMP</h1>
+          <p className=" garchomp-descricao text-xl  ">
             Garchomp (Japonês: ガブリアス Gaburias) é uma espécie de Pokémon dos
             tipos Dragon(Dragão) e Ground (Terrestre), introduzida na 4ª Geração
             e classificada oficialmente como Mach Pokémon. É a forma evoluída de
             Gabite e a forma final de Gible. Garchomp é considerado um Pokémon
             Pseudolendário. Garchomp tem a capacidade de Mega Evolução; quando
             exposto à Mega Stone Garchompite, assume sua forma Mega Garchomp.
-            <p> Fonte:
-              <a
-                className="pt-10 text-5 text-indigo-500 p-2"
-                href="https://victoryroad.fandom.com/wiki/Garchomp"
-              >
-                https://victoryroad.fandom.com/wiki/Garchomp
-              </a>
-            </p>
+          </p>
+          <p className="pt-10 flex items-center">
+            Fonte:
+            <a
+              className=" text-indigo-500 p-2 block text-xl"
+              href="https://victoryroad.fandom.com/wiki/Garchomp"
+            >
+              https://victoryroad.fandom.com/wiki/Garchomp{" "}
+            </a>
           </p>
         </div>
       </div>
-      <img className="w- 1/2 h-[65%]" src={garchomp} alt="garchomp" />
+      <div className="w-[40%] garchomp-img-container">
+        <img className="garchomp-img" src={garchomp} alt="garchomp" />
+      </div>
     </div>
   );
 }

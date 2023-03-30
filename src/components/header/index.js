6cc6cf487777fd7d77813e029d7af84da8475bdb
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-03-30 14:08:22 -0300

    a

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,7 +7,7 @@ export default function Header() {
     <div className="garchomp-container h-screen w-full flex items-center justify-evenly bg-indigo-900">
       <div className="w-[40%] garchomp-info">
         <div className="max-[600px]:text-green-800 ">
-          <h1 className="titulo-garchomp">GARCHOMP</h1>
+          <h1 className="titulo-garchomp text-white">GARCHOMP</h1>
           <p className=" garchomp-descricao text-xl  ">
             Garchomp (Japonês: ガブリアス Gaburias) é uma espécie de Pokémon dos
             tipos Dragon(Dragão) e Ground (Terrestre), introduzida na 4ª Geração
@@ -16,10 +16,10 @@ export default function Header() {
             Pseudolendário. Garchomp tem a capacidade de Mega Evolução; quando
             exposto à Mega Stone Garchompite, assume sua forma Mega Garchomp.
           </p>
-          <p className="pt-10 flex items-center">
+          <p className="pt-10 pl-[10%] pr-[10%] flex items-center">
             Fonte:
             <a
-              className=" text-indigo-500 p-2 block text-xl"
+              className="link text-indigo-500 p-2 text-xl"
               href="https://victoryroad.fandom.com/wiki/Garchomp"
             >
               https://victoryroad.fandom.com/wiki/Garchomp{" "}

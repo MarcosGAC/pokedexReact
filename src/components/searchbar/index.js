d1c53b7d2d3f9d8ca44e89308a9c2cdf674d0af7
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-03-31 15:56:00 -0300

    add filtro por tipo

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import "./search.css";
-import Navbar from "../navbar";
+
+import { pokemonTypeColors } from "../pokemonsStyles/typecolors";
+import typeImages from "../Pokemon/iconesTypes";
 
 const Searchbar = (props) => {
   const [search, setSearch] = useState("");
   const [searching, setSearching] = useState(false);
   const exceptThisSymbols = ["."];
 
-  const { onSearch, searchbarOpen } = props;
+  const { onSearch, searchbarOpen,setSelectedType } = props;
 
   const onChangeHandler = (e) => {
     const result = e.target.value.toLowerCase();
@@ -25,10 +27,31 @@ const Searchbar = (props) => {
     }
   }, [search, onSearch, searching]);
 
+  const options = [
+    { tipo: "flying" },
+    { tipo: "dark" },
+    { tipo: "bug" },
+    { tipo: "ghost" },
+    { tipo: "grass" },
+    { tipo: "dragon" },
+    { tipo: "poison" },
+    { tipo: "fairy" },
+    { tipo: "fighting" },
+    { tipo: "electric" },
+    { tipo: "ground" },
+    { tipo: "ice" },
+    { tipo: "normal" },
+    { tipo: "psychic" },
+    { tipo: "rock" },
+    { tipo: "steel" },
+    { tipo: "water" },
+    { tipo: "fire" },
+  ];
+
   return (
     <>
       {!searchbarOpen ? (
-        <nav className="searchbar-container bg-neutral-900 fixed shadow-lg shadow-zinc-900 w-full justify-evenly">
+        <nav className="searchbar-container shadow-lg shadow-zinc-900 w-full justify-evenly">
           <div className="searchbar-items flex items-center">
             <div className="searchbar">
               <input
@@ -46,7 +69,7 @@ const Searchbar = (props) => {
             </div>
             <div className="searchbar-btn">
               <button
-                className="flex items-center text-[20px]"
+                className="flex items-center text-[20px] "
                 onClick={() => {
                   setSearching(true);
                   onSearch(search);
@@ -55,8 +78,27 @@ const Searchbar = (props) => {
                 Buscar
               </button>
             </div>
+            <div className="">
+            {options.map((opt,index) => {
+              const tipodaimg = typeImages[opt.tipo]
+             
+                    return (
+                      <button
+                      className="m-1 w-20 h-10 rounded-2xl"
+                      key={index}
+                        style={{ backgroundColor: pokemonTypeColors[opt.tipo] }}
+                        value={opt.tipo}
+                        onClick={()=>setSelectedType(opt.tipo)}
+                      >
+                         <div className="flex items-center justify-around">
+                    <img key={index} className="h-6 w-6" src={tipodaimg} alt="icone tipo"/>
+                    {opt.tipo}
+                    </div>
+                      </button>
+                    );
+                  })}
+            </div>      
           </div>
-          <Navbar />
         </nav>
       ) : null}
     </>
